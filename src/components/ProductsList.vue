98c42d797664a3a7c6f0c467d06776d11de3186e
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2018-01-09 22:52:41 +0100

    Create new products function

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -4,6 +4,11 @@
     <ul>
       <li v-for="product in products" >{{ product.name }} - {{ product.price }}zl</li>
     </ul>
+    <form>
+      <input type="text" placeholder="Nazwa produktu" v-model="newProduct.name">
+      <input v-on:keyup.enter="addProduct" type="number" placeholder="Cena produktu" v-model="newProduct.price">
+      <button v-on:click="addProduct" type="button" name="button">Dodaj produkt</button>
+    </form>
   </div>
 </template>
 
@@ -13,18 +18,44 @@ export default {
   data () {
     return {
       products: [],
+      newProduct: {
+        name: '',
+        price: ''
+      }
     }
   },
   created () {
-    axios.get('http://localhost:3000/products')
-    .then( (response) => {
-      if (response.status === 200) {
-        this.products = response.data;
-      }
-    })
-    .catch( (error) => {
-      console.log(error);
-    });
+    this.getProducts();
+  },
+  methods: {
+    getProducts () {
+      axios.get('http://localhost:3000/products')
+      .then( (response) => {
+        if (response.status === 200) {
+          this.products = response.data;
+        }
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
+    },
+    addProduct() {
+      axios.post('http://localhost:3000/products', {
+        name: this.newProduct.name,
+        price: this.newProduct.price
+      })
+      .then( (response) => {
+        if (response.status === 201) {
+          this.getProducts();
+          this.newProduct.name = '';
+          this.newProduct.price = '';
+        }
+        console.log(response);
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
+    }
   }
 }
 
