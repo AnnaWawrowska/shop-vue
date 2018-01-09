97669e2c54cae4c82fe5e8fd120ff4af0701d8fb
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2018-01-09 20:53:24 +0100

    Create Products List component, database file, add axios and create GET request for products

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
+  <div>
+    <productsList></productsList>
+    <shoppingCart></shoppingCart>
   </div>
 </template>
 
 <script>
+import ProductsList from './components/ProductsList';
+import ShoppingCart from './components/ShoppingCart';
+
 export default {
+  components: {
+    'productsList': ProductsList,
+    'shoppingCart': ShoppingCart
+  },
   data () {
     return {
-      msg: 'Hello world'
+
     }
   }
 }
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Lista Produktów :</h1>
+    <ul>
+      <li v-for="product in products" >{{ product.name }} - {{ product.price }}zl</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data () {
+    return {
+      products: [],
+    }
+  },
+  created () {
+    axios.get('http://localhost:3000/products')
+    .then( (response) => {
+      if (response.status === 200) {
+        this.products = response.data;
+      }
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
